6fcca75d8b0aee2b523d53ba4fcc89ba82c834c6
Vegard Langås <sjefen6@gmail.com>
2024-02-18 17:05:11 +0100

    Update Purify name decoration as emoji is unsupported Add shadow& to purification search strings Add screenshot

--- SearchStringGenerator/MarkdownExtensions.cs
+++ SearchStringGenerator/MarkdownExtensions.cs
@@ -2,15 +2,21 @@
 
 public static class MarkdownExtensions
 {
-    public static void AddSearchStringTable(this StringBuilder sb, string title, Dictionary<string, string> searchStrings, Func<string, string> nameDecorator)
+    public static void AddSearchStringTable(
+        this StringBuilder sb, 
+        string title, 
+        Dictionary<string, string> searchStrings, 
+        Func<string, string> nameDecorator, 
+        Func<string, string>? searchDecorator = null)
     {
+        searchDecorator ??= x => x;
         sb.AppendLine($"## {title}")
             .AppendLine("| Name | Search string |")
             .AppendLine("|------|---------------|");
 
         foreach (var item in searchStrings)
         {
-            sb.AppendLine($"| `{nameDecorator(item.Key)}` | `{item.Value}` |");
+            sb.AppendLine($"| `{nameDecorator(item.Key)}` | `{searchDecorator(item.Value)}` |");
         }
     }
 }
--- SearchStringGenerator/Program.cs
+++ SearchStringGenerator/Program.cs
@@ -3,7 +3,7 @@
 var markdown = new StringBuilder();
 markdown.AppendLine("# Pokemon GO search strings")
         .AppendLine("This project reads the game master file and generates search strings for Pokemon GO."
-                + " The search strings can be used to filter Pokemon using the in the game search.")
+                    + " The search strings can be used to filter Pokemon using the in the game search.")
         .AppendLine("## Usage")
         .AppendLine("Copy the search string and paste it in the search bar in Pokemon GO.")
         .AppendLine()
@@ -11,10 +11,12 @@ markdown.AppendLine("# Pokemon GO search strings")
         .AppendLine(" 1. Clear the search string from the search bar")
         .AppendLine(" 1. [See more] to reveal Recent searches")
         .AppendLine(" 1. Long press on the search string under Recent to add it to favorites")
-        .AppendLine(" 1. Long press on the search string under Favorites and give the search a name");
+        .AppendLine(" 1. Long press on the search string under Favorites and give the search a name")
+        .AppendLine()
+        .AppendLine("![Screenshot from Favorite searches inside the game with all the search strings added](screenshot.png)");
 
 markdown.AddSearchStringTable("BuddyDistance", pokemons.DistanceSearchStrings(), x => $"Buddy {x} km");
-markdown.AddSearchStringTable("PurificationStardustNeeded", pokemons.PurificationStardustSearchStrings(), x => $"Purify {x} ✨");
+markdown.AddSearchStringTable("PurificationStardustNeeded", pokemons.PurificationStardustSearchStrings(), x => $"Purify {x}", x => $"shadow&{x}");
 
 markdown.AppendLine("# Regenerating the README.md file")
         .AppendLine("To regenerate the README.md file, run the `SearchStringGenerator` project.")
